7ea458acb07c96526db184b47ba81f9e017f4935
Darshan Raiyani <darshanraiyani63@gmail.com>
2026-08-17 20:22:35 +0530

    Done Databs configuration

---
+++ backend/config/db.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose')
+
+const connectDB = async ()=>{
+    try{
+        await mongoose.connect(process.env.MONGO_URI)
+        console.log("database connected !");
+        
+    }
+    catch(error){
+        console.error("Mongobd connection error:" ,error.message);
+        
+    }
+}
